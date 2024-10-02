ca8caa51fed90158043bd13ac3d29232c81f7858
Tenergy Innovation <45764087+tenergyinnovation@users.noreply.github.com>
2024-10-02 09:47:53 +0700

    Update tiny32 interface with sd card via SPI bus  example code
    Update tiny32 interface with sd card via SPI bus  example code

--- README.md
+++ README.md
@@ -1,32 +1,8 @@
-# tiny32_SDCard
-Example code tiny32 interface with SD card via SPI bus
+# tiny32_v3_temploate_vscode
 
-*********************************************************************
+*** Template for tiny32_v3 development on vscode ***
 
+*** Before run this example, you must install tiny32 library first at below link ***
+1. go to lib folder in this project 
+2. git clone https://github.com/tenergyinnovation/tiny32_v3.git
 
-##### Library need to clone from github ####
-1. git clone https://github.com/tenergyinnovation/tiny32_v3.git
-
-2. git clone https://github.com/arduino-libraries/SD.git
-   - comment file "Sd2PinMap.h" at line 527 => //#error Architecture or board not supported. 
-
-
-#### Hardware configuration ####
-*** SPI pin between tiny32 to SD card ****
-
-MISO --> 19
-
-MOSI --> 23
-
-SCK  --> 18
-
-CS   --> 5 
-
-
-
-#### Hardware Link ####
-https://www.allnewstep.com/product/557/%E0%B9%82%E0%B8%A1%E0%B8%94%E0%B8%B9%E0%B8%A5%E0%B8%9A%E0%B8%B1%E0%B8%99%E0%B8%97%E0%B8%B6%E0%B8%81%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%A5%E0%B8%87-micro-sd-card-micro-sd-card-module-micro-sd-card-module
-
-https://randomnerdtutorials.com/esp32-microsd-card-arduino/
-
-https://microcontrollerslab.com/microsd-card-esp32-arduino-ide/
---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+/***********************************************************************
+ * Project      :     Example_Datalogger
+ * Description  :     This example shows how to log data from three analog sensors
+                      to an SD card using the SD library.
+ * Hardware     :     tiny32_v3
+ * Author       :     Tenergy Innovation Co., Ltd.
+ * Date         :     04/07/2022
+ * Revision     :     1.0
+ * Rev1.0       :     Origital
+ * website      :     http://www.tenergyinnovation.co.th
+ * Email        :     admin@innovation.co.th
+ * TEL          :     +66 82-380-3299
+ ***********************************************************************/
+#include <Arduino.h>
+#include <tiny32_v3.h>
+#include <SPI.h>
+#include <SD.h>
+
+tiny32_v3 mcu;
+
+const int chipSelect = 5;
+
+boolean sdcard_flag = 0;
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  int _error_cnt = 0;
+
+  Serial.printf("*** tiny32 Example_Datalogger project ***\r\n");
+  mcu.library_version();
+
+ /************ SD card Initial *************/
+  Serial.print("Info: Initializing SD card...");
+  do
+  {
+    sdcard_flag = SD.begin(chipSelect);
+    _error_cnt++;
+    Serial.printf("# ");
+    vTaskDelay(1000);
+  } while (!sdcard_flag && (_error_cnt < 5));
+
+  if (!sdcard_flag)
+  {
+    Serial.println("--> failed\r\n");
+    Serial.println("\r\nError: !!initialization failed. Things to check: --> fail");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset or reopen this serial monitor after fixing your issue!");
+    mcu.buzzer_beep(4);
+    while(1);
+  }
+  else
+  {
+    Serial.println("--> passed");
+    mcu.buzzer_beep(1);
+    vTaskDelay(1000);
+  }
+  Serial.println("card initialized.");
+
+  mcu.buzzer_beep(2);
+}
+
+void loop()
+{
+
+  String _dataString = "";
+  int _sensor_value;
+
+  for (int i = 0; i < 10; i++)
+  {
+    _sensor_value = analogRead(33); // Analog read from PIN33 ;
+    _dataString += String(_sensor_value);
+    if (i < 9)
+    _dataString += ",";
+    vTaskDelay(100);
+  }
+
+  
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile)
+  {
+    dataFile.println(_dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(_dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else
+  {
+    Serial.println("error opening datalog.txt");
+  }
+}
